7071f230fa64ff4b0764bfb209c7f194603f7588
doubleq2 <90964028+doubleq2@users.noreply.github.com>
2023-10-20 01:05:34 +0700

    Update env.py

--- source/db/migrations/env.py
+++ source/db/migrations/env.py
@@ -87,11 +87,6 @@ def do_run_migrations(connection: Connection) -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
-# if context.is_offline_mode():
-#     run_migrations_offline()
-# else:
-#     run_migrations_online()
 async def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
