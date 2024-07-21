7d78d3d899979f3a9bc19a7edf63790665f7ba44
Yuriy Ilchuk <yuryilchuk@yandex.ru>
2024-07-21 17:19:14 +0700

    fin producer logic

--- source/api/services/service.py
+++ source/api/services/service.py
@@ -38,20 +38,25 @@ class MenuService(BaseService):
         menu_data = await repository.create(title=menu_schema.title, description=menu_schema.description)
 
         producer.produce('menu_topic', key=menu_data['id'], value=json.dumps(menu_data).encode('utf-8'))
-        producer.flush()
 
         return JSONResponse(content=menu_data, status_code=status.HTTP_201_CREATED)
-
-    async def delete(self, menu_id: UUID) -> JSONResponse:
-        await clear_cache()
-        repository = await RepositoryFactory.create('menu', self.db)
-        menu_data = await repository.delete(id=menu_id)
-        return JSONResponse(content=menu_data, status_code=status.HTTP_200_OK)
 
     async def update(self, menu_id: UUID, menu_schema: MenuCreate) -> JSONResponse:
         await clear_cache()
         repository = await RepositoryFactory.create('menu', self.db)
         menu_data = await repository.update(id=menu_id, title=menu_schema.title, description=menu_schema.description)
+
+        producer.produce('menu_topic', key=str(menu_data['id']), value=json.dumps(menu_data).encode('utf-8'))
+
+        return JSONResponse(content=menu_data, status_code=status.HTTP_200_OK)
+
+    async def delete(self, menu_id: UUID) -> JSONResponse:
+        await clear_cache()
+        repository = await RepositoryFactory.create('menu', self.db)
+        menu_data = await repository.delete(id=menu_id)
+
+        producer.produce('menu_topic', key=str(menu_id), value=None)
+
         return JSONResponse(content=menu_data, status_code=status.HTTP_200_OK)
 
 
@@ -79,6 +84,9 @@ class SubMenuService(BaseService):
         repository = await RepositoryFactory.create('submenu', self.db)
         submenu_data = await repository.create(menu_id=menu_id, title=submenu_schema.title,
                                                description=submenu_schema.description)
+
+        producer.produce('submenu_topic', key=str(submenu_data['id']), value=json.dumps(submenu_data).encode('utf-8'))
+
         return JSONResponse(content=submenu_data, status_code=status.HTTP_201_CREATED)
 
     async def update(self, submenu_id: UUID, submenu_schema: SubMenuCreate) -> JSONResponse:
@@ -86,12 +94,18 @@ class SubMenuService(BaseService):
         repository = await RepositoryFactory.create('submenu', self.db)
         submenu_data = await repository.update(id=submenu_id, title=submenu_schema.title,
                                                description=submenu_schema.description)
+
+        producer.produce('submenu_topic', key=str(submenu_data['id']), value=json.dumps(submenu_data).encode('utf-8'))
+
         return JSONResponse(content=submenu_data, status_code=status.HTTP_200_OK)
 
     async def delete(self, submenu_id: UUID) -> JSONResponse:
         await clear_cache()
         repository = await RepositoryFactory.create('submenu', self.db)
         submenu_data = await repository.delete(submenu_id)
+
+        producer.produce('submenu_topic', key=str(submenu_id), value=None)
+
         return JSONResponse(content=submenu_data, status_code=status.HTTP_200_OK)
 
 
@@ -119,6 +133,9 @@ class DishService(BaseService):
         repository = await RepositoryFactory.create('dish', self.db)
         dish_data = await repository.create(submenu_id=submenu_id, title=dish_schema.title,
                                             price=dish_schema.price, description=dish_schema.description)
+
+        producer.produce('dish_topic', key=str(dish_data['id']), value=json.dumps(dish_data).encode('utf-8'))
+
         return JSONResponse(content=dish_data, status_code=status.HTTP_201_CREATED)
 
     async def update(self, dish_id: UUID, dish_schema: DishUpdate) -> JSONResponse:
@@ -126,10 +143,16 @@ class DishService(BaseService):
         repository = await RepositoryFactory.create('dish', self.db)
         dish_data = await repository.update(title=dish_schema.title, price=dish_schema.price,
                                             description=dish_schema.description, id=dish_id)
+
+        producer.produce('dish_topic', key=str(dish_data['id']), value=json.dumps(dish_data).encode('utf-8'))
+
         return JSONResponse(content=dish_data, status_code=status.HTTP_200_OK)
 
     async def delete(self, dish_id: UUID) -> JSONResponse:
         await clear_cache()
         repository = await RepositoryFactory.create('dish', self.db)
         dish_data = await repository.delete(dish_id)
+
+        producer.produce('dish_topic', key=str(dish_data['id']), value=None)
+
         return JSONResponse(content=dish_data, status_code=status.HTTP_200_OK)
